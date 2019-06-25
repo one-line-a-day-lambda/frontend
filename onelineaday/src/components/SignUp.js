c2bfbe66d946871dddc5702b7db0f95a476f42e8
Zolton <gmzesq@gmail.com>
2019-06-25 11:55:29 -0400

    Added outline of username and password forms

--- onelineaday/src/components/SignUp.js
+++ onelineaday/src/components/SignUp.js
@@ -1,16 +1,33 @@
-import React from "react"
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default class SignUp extends React.Component {
+  // Where I plan to put login and most of the authorization stuff
 
-// Where I plan to put login and most of the authorization stuff
+  //changeHandler = event => {
+  //   preventDefault();
+  // this.setState({
+  //     [event.target.name]: event.target.value
+  // })
+  //}
 
-
-    render(){
-        return(
-            <div>
-                Hello from Signup
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        Hello from Signup
+        <input
+          type="text"
+          placeholder="Username"
+          //value={this.state.username}
+          //onChange={this.changeHandler}
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          //value={this.state.password}
+          //onChange={this.changeHandler}
+        />
+      </div>
+    );
+  }
 }
