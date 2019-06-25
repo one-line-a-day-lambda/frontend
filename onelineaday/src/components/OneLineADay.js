9e8ef6449d63806bd3fae33236d085724a2ba750
Zolton <gmzesq@gmail.com>
2019-06-25 15:13:57 -0400

    Back to the drawing board

--- onelineaday/src/components/OneLineADay.js
+++ onelineaday/src/components/OneLineADay.js
@@ -1,46 +1,43 @@
-import React from "react"
-import axios from "axios"
-
+import React from "react";
+import axios from "axios";
 
 export default class OneLineADay extends React.Component {
-
-    // state of state a total guess.  No idea what server looks like right now
-    // Use Date.now() for both date and post ID - two birds with one stone
-    state = {
-        diaryEntry: {
-            post:"",
-            id: Date.now(),
-        }
-    }
-    // Just get it rendering first.  Later spin this off into its own (class?) component
-    // and use an imported callback in the componentDidMount
-   componentDidMount(){ 
+  // state of state a total guess.  No idea what server looks like right now
+  // Use Date.now() for both date and post ID - two birds with one stone
+  state = {
+    post: ""
+  };
+  // Just get it rendering first.  Later spin this off into its own (class?) component
+  // and use an imported callback in the componentDidMount
+  componentDidMount() {
     axios
-    .get("https://onelineadaybe.herokuapp.com/")
-    .then(res=>{
-        console.log(res)
-      //  this.setState({post=})
-    })
-    .catch(rej=>{
-        console.log(rej)
-    })
-}
+      .get("https://onelineadaybe.herokuapp.com/")
+      .then(
+        res => (
+          console.log(res),
+          console.log(res.data),
+          this.setState({ post: res.data }),
+          console.log(this.state.post),
+          console.log(this.state.post.message)
+        )
+      )
+      .catch(rej => console.log(rej));
+  }
 
-    render(){
-        // Again, just get it working
-        // Later, spin map off into its own (fucntion, not class) component, use OneLineADay for rendering only
-        return (
-            <div>
-                {/* {this.state.diaryEntry.map(post=>{
+  render() {
+    // Again, just get it working
+    // Later, spin map off into its own (fucntion, not class) component, use OneLineADay for rendering only
+    return (
+      <div>
+        {/* {this.state.diaryEntry.map(post=>{
                 <div>
                     <h4>{post.id}</h4>
                     <h4>{post.post}</h4>
                 </div>
             })} */}
-            
-                Hello from OneLineADay
-            </div>
-
-        )
-    }
+        Hello from OneLineADay
+        <p> {this.state.post.message}</p>
+      </div>
+    );
+  }
 }
